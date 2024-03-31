c5a4b57f2666c3df0ac36899162cc15d97b6d0e9
Mofe-banks01 <bankoleadedotun16@gmail.com>
2024-04-01 00:00:09 +0100

    Responsiveness Update

---
+++ script.js
@@ -0,0 +1,32 @@
+// First we need to get the DOm elements of all input/options so taht we can implement them into our code
+// DOM Elements
+
+// Function TO Convert Weight
+function convertWeight() {
+  const userInput = document.getElementById("userinput");
+  const unit = document.getElementById("unitSelect");
+  const resultText = document.getElementById("result");
+  // Resultvalue
+  let result = null;
+  // Collecting Userinput
+  const uservalueInput = userInput.value;
+  // Checking for input , and selected unit.
+  if (!uservalueInput) {
+    alert(`Please Input A Value`);
+    resultText.innerText = `You are yet to put in a value`;
+  } else if (unit.value === "kg") {
+    result = uservalueInput * 0.453592;
+  } else if (unit.value === "lb") {
+    result = uservalueInput * 2.204623;
+  } else if (unit.value === "mg") {
+    result = uservalueInput * 1000000;
+  } else if (unit.value === "dg") {
+    result = uservalueInput * 100;
+  } else if (unit.value === "cg") {
+    result = uservalueInput * 100000;
+  }
+  console.log(uservalueInput);
+  console.log(result);
+  if (uservalueInput !== 0)
+    resultText.innerText = `Result : ${result} ${unit.value}`;
+}
